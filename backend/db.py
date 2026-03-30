4c6c27429bd1d3d58bc33ed698c87648297855a7
AtharvaVavhal <atharvavavhal@gmail.com>
2026-03-30 16:45:10 +0530

    fix

--- backend/db.py
+++ backend/db.py
@@ -3,7 +3,9 @@ import os
 
 DATABASE = os.environ.get("DATABASE_PATH", "habits.db")
 
+
 def get_db():
+    print("DB PATH:", DATABASE)
     conn = sqlite3.connect(DATABASE)
     conn.row_factory = sqlite3.Row
     conn.execute("PRAGMA foreign_keys = ON")
